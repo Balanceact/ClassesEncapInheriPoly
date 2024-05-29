c5bb4a6d7594bb9d677f0afb5caa8bdb36b23ebe
Balanceact <makildur@hotmail.com>
2024-05-29 12:28:06 +0200

    Add project files.

---
+++ Classes_Encap_Inheri_Poly/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Classes_Encap_Inheri_Poly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+        }
+    }
+}
