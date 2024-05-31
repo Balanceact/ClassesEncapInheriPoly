33189dd462c0b71113a649816edace4b61552503
Balanceact <makildur@hotmail.com>
2024-05-31 16:33:50 +0200

    3.1.3 implemented.

--- Classes_Encap_Inheri_Poly/Program.cs
+++ Classes_Encap_Inheri_Poly/Program.cs
@@ -8,9 +8,11 @@ namespace Classes_Encap_Inheri_Poly
         {
             try
             {
-                Person testPerson1 = new Person("Patrik", "Persson", 35, 175, 80);
-                Person testPerson2 = new Person("Peter", "Persson", 32);
-                Person testperson3 = new Person("Hildar", "Johansson", 35, 167, 75);
+                //Person testPerson1 = new Person("Patrik", "Persson", 35, 175, 80);
+                //Person testPerson2 = new Person("Peter", "Persson", 32);
+                //Person testPerson3 = new Person("Hildar", "Johansson", 35, 167, 75);
+                PersonHandler personHandler = new PersonHandler();
+                Person testPerson4 = personHandler.CreatePerson("Hurtig", "Bulle", 43, 178, 70);
             }
             catch (Exception Ex)
             {
