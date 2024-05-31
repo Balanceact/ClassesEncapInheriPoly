da1c5e6c5c9a8a38eea00d268b421546655d6ea1
Balanceact <makildur@hotmail.com>
2024-05-31 19:38:42 +0200

    3.1.4 implemented.

--- Classes_Encap_Inheri_Poly/PersonHandler.cs
+++ Classes_Encap_Inheri_Poly/PersonHandler.cs
@@ -8,11 +8,27 @@ namespace Classes_Encap_Inheri_Poly
 {
     internal class PersonHandler
     {
+        public void SetFName(Person pers, string fName)
+        {
+            pers.FName = fName;
+        }
+        public void SetLName(Person pers, string lName)
+        {
+            pers.LName = lName;
+        }
         public void SetAge(Person pers, int age)
         {
             pers.Age = age;
         }
-
+        public void SetHeight(Person pers, double height)
+        {
+            pers.Height = height;
+        }
+        public void SetWeight(Person pers, double weight)
+        {
+            pers.Weight = weight;
+        }
+        
         public Person CreatePerson(string fname, string lname, int age, double height, double weight)
         {
             return new Person(fname, lname, age, height, weight);
