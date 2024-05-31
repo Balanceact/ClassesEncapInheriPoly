369306a6044080f7fd7ac418d7a14390a01d722c
Balanceact <makildur@hotmail.com>
2024-05-31 10:13:05 +0200

    Try , Catch implemented.

--- Classes_Encap_Inheri_Poly/Program.cs
+++ Classes_Encap_Inheri_Poly/Program.cs
@@ -4,8 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Person testPerson1 = new Person("Patrik", "Person", 35, 175, 80);
-            Person testPerson2 = new Person("Peter", "Person", 32);
+            try
+            {
+                Person testPerson1 = new Person("Patrik", "Person", 35, 175, 80);
+                Person testPerson2 = new Person("Peter", "Person", 32);
+            }
+            catch (ArgumentException)
+            {
+
+            }
+            finally
+            {
+
+            }
         }
     }
 }
