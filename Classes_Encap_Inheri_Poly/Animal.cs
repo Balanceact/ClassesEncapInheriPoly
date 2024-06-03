cc2afe5fa3c0aab5c3c257e0f6b377338b8ce959
Balanceact <makildur@hotmail.com>
2024-06-03 09:38:34 +0200

    3.3.5-6 implemented.

--- Classes_Encap_Inheri_Poly/Animal.cs
+++ Classes_Encap_Inheri_Poly/Animal.cs
@@ -11,14 +11,43 @@ namespace Classes_Encap_Inheri_Poly
         protected string Name { get; set; }
         protected int Age { get; set; }
         protected double Weight { get; set; }
+        protected double LongestDimension { get; set; }
+        protected int NumberOfLimbs { get; set; }
 
-        protected Animal(string name, int age, double weight)
+        protected Animal(string name, int age, double weight, double longestDimension, int numberOfLimbs)
         {
             Name = name;
             Age = age;
             Weight = weight;
+            LongestDimension = longestDimension;
+            NumberOfLimbs = numberOfLimbs;
         }
 
         public abstract void doSound();
+    }
+
+    internal class Horse : Animal
+    { 
+        public bool IsShoed { get; set; }
+    }
+    internal class Dog : Animal
+    {
+        public bool LikesToBark { get; set; }
+    }
+    internal class Hedgehog : Animal
+    {
+        public int NumberOfSpikes { get; set; }
+    }
+    internal class Worm : Animal
+    {
+        public bool IsPoisonus { get; set; }
+    }
+    internal class Bird : Animal
+    {
+        public double WingSpan { get; set; }
+    }
+    internal class Wolf : Animal
+    {
+        public bool LikesToHowl { get; set; }
     }
 }
