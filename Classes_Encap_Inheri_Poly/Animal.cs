3aef8772c94e3d6eebf929e577007840d434d83f
Balanceact <makildur@hotmail.com>
2024-06-03 09:15:12 +0200

    3.3.1-4 impelemnted.

---
+++ Classes_Encap_Inheri_Poly/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Encap_Inheri_Poly
+{
+    internal abstract class Animal
+    {
+        protected string Name { get; set; }
+        protected int Age { get; set; }
+        protected double Weight { get; set; }
+
+        protected Animal(string name, int age, double weight)
+        {
+            Name = name;
+            Age = age;
+            Weight = weight;
+        }
+
+        public abstract void doSound();
+    }
+}
